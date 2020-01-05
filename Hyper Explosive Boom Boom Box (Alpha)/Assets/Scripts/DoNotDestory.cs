3ff15aac44df416df3f8cd355cf69a2acbb85377
Stephen Sandlin <info@SandlinProds.com>
2020-01-05 11:16:13 -0500

    Master_GameManager

--- Hyper Explosive Boom Boom Box (Alpha)/Assets/Scripts/DoNotDestory.cs
+++ Hyper Explosive Boom Boom Box (Alpha)/Assets/Scripts/DoNotDestory.cs
@@ -2,28 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Abilities : MonoBehaviour
+public class DoNotDestory : MonoBehaviour
 {
-    public bool dash;
-    public bool timeShift;
-    public bool wallStick;
-    //public static Abilities instance = null;
-
-    void Awake()
+    public static DoNotDestory instance = null;
+    private void Awake()
     {
-        dash = false;
-        timeShift = false;
-        wallStick = false;
         //Check if there is already an instance of SoundManager
-        //if (instance == null)
+        if (instance == null)
             //if not, set it to this.
-          //  instance = this;
+            instance = this;
         //If instance already exists:
-        //else if (instance != this)
+        else if (instance != this)
             //Destroy this, this enforces our singleton pattern so there can only be one instance of SoundManager.
-          //  Destroy(gameObject);
+            Destroy(gameObject);
 
         //Set SoundManager to DontDestroyOnLoad so that it won't be destroyed when reloading our scene.
-       // DontDestroyOnLoad(gameObject);
+        DontDestroyOnLoad(gameObject);
     }
 }

0f00cd0f59d2d1d045f65603fe3a511f0971b291
Stephen Sandlin <info@SandlinProds.com>
2020-02-08 13:12:11 -0500

    Unity Update 2019.3
    Updated Unity and added collectables

--- Hyper Explosive Boom Boom Box (Alpha)/Assets/Scripts/CollectableUpdating.cs
+++ Hyper Explosive Boom Boom Box (Alpha)/Assets/Scripts/CollectableUpdating.cs
@@ -18,6 +18,7 @@ public class CollectableUpdating : MonoBehaviour
     public PlayerController playerController;
     public GameObject particle;
     public GameObject SoundManager;
+    public float time;
 
     private void Start()
     {
@@ -32,21 +33,26 @@ public class CollectableUpdating : MonoBehaviour
             {
                 float xTarget = trackingTarget.position.x + xOffset;
                 float yTarget = trackingTarget.position.y + yOffset;
+                float zTarget = trackingTarget.position.z;
 
                 float xNew = Mathf.Lerp(transform.position.x, xTarget, Time.deltaTime * followSpeed);
                 float yNew = Mathf.Lerp(transform.position.y, yTarget, Time.deltaTime * followSpeed);
+                float zNew = Mathf.Lerp(transform.position.z, zTarget, Time.deltaTime * followSpeed);
+             
 
-                transform.position = new Vector3(xNew, yNew, transform.position.z);
+                transform.position = new Vector3(xNew, yNew, zNew);
             }
             else if (playerController.facingRight == false)
             {
                 float xTarget = trackingTarget.position.x + -xOffset;
                 float yTarget = trackingTarget.position.y + yOffset;
+                float zTarget = trackingTarget.position.z;
 
                 float xNew = Mathf.Lerp(transform.position.x, xTarget, Time.deltaTime * followSpeed);
                 float yNew = Mathf.Lerp(transform.position.y, yTarget, Time.deltaTime * followSpeed);
+                float zNew = Mathf.Lerp(transform.position.z, zTarget, Time.deltaTime * followSpeed);
 
-                transform.position = new Vector3(xNew, yNew, transform.position.z);
+                transform.position = new Vector3(xNew, yNew, zNew);
             }
             particle.transform.RotateAround(Vector3.zero, Vector3.up, followSpeed * Time.deltaTime);
         }
@@ -57,20 +63,29 @@ public class CollectableUpdating : MonoBehaviour
     {
         if(collision.gameObject.tag == "Player")
         {
-            GetComponent<BoxCollider>().isTrigger = true;
+            //GetComponent<MeshCollider>().isTrigger = true;
             SoundManager.GetComponent<SoundManager>().RandomizeSfx(collectingSound);
             collected = true;
             CollectableScoring.theScore += 1;
             StartCoroutine(Collected());
         }
-        
+       
     }
-
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            SoundManager.GetComponent<SoundManager>().RandomizeSfx(collectingSound);
+            collected = true;
+            CollectableScoring.theScore += 1;
+            StartCoroutine(Collected());
+        }
+    }
     IEnumerator Collected()
     {
         Debug.Log("Starting");
-        yield return new WaitForSeconds(.15f);
-        Instantiate(particle, transform.position, Quaternion.identity);
+        yield return new WaitForSeconds(time);
+        //Instantiate(particle, transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
 }

b35bbb7033a63f7b29154b0497eb77b630b4b032
Stephen Sandlin <info@SandlinProds.com>
2020-05-02 10:31:22 -0400

    Level 6 update

---
+++ Hyper Explosive Boom Boom Box (Alpha)/Assets/Scripts/BladeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BladeController : MonoBehaviour
+{
+    public ParticleSystem sparks;
+    private void OnCollisionStay(Collision collision)
+    {
+        CreateSparks();
+    }
+    
+
+    void CreateSparks()
+    {
+        sparks.Play();
+        print("I'm making sparks");
+    }
+}

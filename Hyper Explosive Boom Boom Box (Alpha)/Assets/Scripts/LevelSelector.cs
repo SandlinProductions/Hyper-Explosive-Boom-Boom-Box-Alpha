cb1535bbe25396c9448196f314d07f948608c4f4
Stephen Sandlin <info@SandlinProds.com>
2020-10-31 12:20:56 -0400

    WorldMap Level Selector added

---
+++ Hyper Explosive Boom Boom Box (Alpha)/Assets/Scripts/LevelSelector.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelector : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject levelSelectorUI;
+    public string levelName;
+
+    private void Start()
+    {
+        levelSelectorUI.SetActive(false);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject == player)
+        {
+            Debug.Log("I'm touching the Level Selector");
+            levelSelectorUI.SetActive(true);
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if(collision.gameObject == player)
+        {
+            Debug.Log("I stepped off the Level Selector");
+            levelSelectorUI.SetActive(false);
+        }
+    }
+}
